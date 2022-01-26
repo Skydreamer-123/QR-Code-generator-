c9416b15f0caed88cbfca7674b031b33a5b22da3
Skydreamer-123 <danielungar1@gmx.de>
2022-01-26 19:49:06 +0100

    changing size and adding features to the generator

--- qrCodeGenerator.py
+++ qrCodeGenerator.py
@@ -4,9 +4,16 @@ with open ("link.txt", "r") as file:
     file = open("link.txt","r")
 for line in file:
     x = line.rstrip()
-    img = qrcode.make(x)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=4,
+        border=4,
+    )
+    qr.add_data(x)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
     imgName = x.split("#/lernkoffer/")[1].replace("/","-") + ".png"
     img.save(imgName)
 
-file.close()
-    
+
